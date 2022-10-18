4fcf2689b88ceb6b71dfa0fe1b6049e689be2723
Kaitlyn Biehler <kmbiehle@buffalo.edu>
2022-10-18 17:39:50 -0400

    case study 7 updates

---
+++ week_07/CS_07_nocomments.R
@@ -0,0 +1,32 @@
+#case study 7
+#Getting Help!
+
+#load required libraries
+library(tidyverse)
+library(sf)
+library(spData)
+
+#use data from sp world
+data(world)
+
+ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
+   geom_density(alpha=0.5,color=F)
+########
+#creating the reprex
+#copy the code you ran above (that didn't work) before you run the next step
+
+#library(reprex)
+#reprex(venue = "gh")
+
+#this is what you get
+# library(tidyverse)
+# library(sf)
+# #> Linking to GEOS 3.10.2, GDAL 3.4.2, PROJ 8.2.1; sf_use_s2() is TRUE
+# library(spData)
+# 
+# #use data from sp world
+# data(world)
+# 
+# ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
+#   geom_density(alpha=0.5,color=F)
+# #> Warning: Removed 17 rows containing non-finite values (stat_density).
